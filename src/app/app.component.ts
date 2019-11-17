a390cdbc8d19b7cbe36bc7dbf91b7a3d5ceeaefd
Muhammad Junaid <junaid1460@gmail.com>
2019-11-17 17:43:04 +0530

    Boilerplate populating

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SearchService } from './search.service';
+import { Field, Filter } from 'src/@types';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'clarisights';
+export class AppComponent implements OnInit {
+    title = 'clarisights';
+    fields: Field[] = [];
+    filters: Filter[] = [
+        {
+            lhs: 'account',
+            operator: '>=',
+            rhs: 'red',
+        },
+        {
+            lhs: 'account',
+            operator: '>=',
+            rhs: 'red',
+        },
+        {
+            lhs: 'account',
+            operator: '>=',
+            rhs: 'red',
+        },
+    ];
+    constructor(private search: SearchService) {
+        console.log('Bootup');
+    }
+
+    async ngOnInit() {
+        const fetchedFields = await this.search.getFilterFields();
+        this.fields.push(...fetchedFields);
+    }
 }
