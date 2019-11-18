7a8eca60bcd034010bcf07a24fa959718d124c73
Muhammad Junaid <junaid1460@gmail.com>
2019-11-18 21:00:03 +0530

    define operator constants

---
+++ src/app/constants.ts
@@ -0,0 +1,65 @@
+import { Operator, Field } from 'src/@types';
+
+export const operators = {
+    EQUALS: {
+        code: '=',
+        display: '=',
+        allowMultiValues: false,
+    },
+    GREATER_THAN: {
+        code: '>',
+        display: '>',
+        allowMultiValues: false,
+    },
+    GREATER_THAN_OR_EQUALS: {
+        code: '>=',
+        display: '>=',
+        allowMultiValues: false,
+    },
+    LESS_THAN: {
+        code: '<',
+        display: '<',
+        allowMultiValues: false,
+    },
+    LESS_THAN_OR_EQUALS: {
+        code: '<=',
+        display: '<=',
+        allowMultiValues: false,
+    },
+    NOT_EQUALS: {
+        code: '!=',
+        display: '!=',
+        allowMultiValues: false,
+    },
+    STARTS_WITH: {
+        code: 'starts_with',
+        display: 'Starts With',
+        allowMultiValues: false,
+    },
+    CONTAINS: {
+        code: 'contains',
+        display: 'Contains',
+        allowMultiValues: true,
+    },
+    NOT_CONTAINS: {
+        code: 'not_contains',
+        display: 'Not Contains',
+        allowMultiValues: true,
+    },
+};
+
+export const operatorsByDataType: {
+    [type in Field['type']]: Operator[];
+} = {
+    decimal: [
+        operators.EQUALS,
+        operators.GREATER_THAN,
+        operators.GREATER_THAN_OR_EQUALS,
+        operators.LESS_THAN,
+        operators.LESS_THAN_OR_EQUALS,
+    ],
+    integer: [operators.CONTAINS, operators.NOT_CONTAINS],
+    text: [operators.STARTS_WITH, operators.CONTAINS, operators.NOT_CONTAINS],
+};
+
+export const allOperators = [operators.EQUALS, operators.STARTS_WITH, operators.CONTAINS];
