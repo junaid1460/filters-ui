c52b809b0b8d7501a431cbfb15c768787b7cd952
Muhammad Junaid <junaid1460@gmail.com>
2019-11-18 20:59:44 +0530

    import required material modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,33 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+    MatButtonModule,
+    MatIconModule,
+    MatSelectModule,
+    MatMenuModule,
+    MatInputModule,
+    MatListModule,
+} from '@angular/material';
+import { FilterComponent } from './filter/filter.component';
+import { InputWidgetComponent } from './input-widget/input-widget.component';
+import { DecimalInputDirective } from './decimal-input.directive';
 
 @NgModule({
-    declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule, BrowserAnimationsModule],
+    declarations: [AppComponent, FilterComponent, InputWidgetComponent, DecimalInputDirective],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule,
+        MatButtonModule,
+        MatIconModule,
+        MatSelectModule,
+        MatMenuModule,
+        MatInputModule,
+        MatListModule,
+    ],
     providers: [],
     bootstrap: [AppComponent],
 })
