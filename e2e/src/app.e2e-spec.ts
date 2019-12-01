46d564460acd3579e26e7ea3cd8724d967b48123
Muhammad Junaid <junaid1460@gmail.com>
2019-12-01 19:28:00 +0530

    add flow to cover different aspects of application

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -2,22 +2,124 @@ import { AppPage } from './app.po';
 import { browser, logging } from 'protractor';
 
 describe('workspace-project App', () => {
-  let page: AppPage;
+    let page: AppPage;
 
-  beforeEach(() => {
-    page = new AppPage();
-  });
+    beforeEach(() => {
+        page = new AppPage();
+    });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to clarisights!');
-  });
+    it('Add button should be there', () => {
+        page.navigateTo();
+        expect(page.getAddButton()).toEqual('+ ADD');
+    });
 
-  afterEach(async () => {
-    // Assert that there are no errors emitted from the browser
-    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-    expect(logs).not.toContain(jasmine.objectContaining({
-      level: logging.Level.SEVERE,
-    } as logging.Entry));
-  });
+    it('Add button should be there', () => {
+        page.navigateTo();
+        expect(page.getApplyButton()).toEqual('APPLY');
+    });
+
+    it('Clicking add should add one more filter', () => {
+        const timegap = 300;
+        let filter = 1;
+        page.navigateTo();
+        browser.sleep(timegap);
+        page.clickAddButton();
+        browser.sleep(timegap);
+        page.clickNthFilterFieldSelector(1);
+        browser.sleep(timegap);
+        page.selectNthMaterialOption(1);
+        browser.sleep(timegap);
+        page.clickNthFilterOperatorSelector(1);
+        browser.sleep(timegap);
+        page.selectNthMaterialOption(1);
+        browser.sleep(timegap);
+        page.clickNthFilterRHSSelector(1);
+        browser.sleep(timegap);
+        page.addSuggestionAndSelect(1, 10233);
+        browser.sleep(timegap);
+        page.selectNthRHS(1, 2);
+        browser.sleep(timegap);
+        page.clickApply(1);
+        browser.sleep(timegap);
+
+        page.clickNthFilterFieldSelector(2);
+        browser.sleep(timegap);
+        page.selectNthMaterialOption(2);
+        browser.sleep(timegap);
+        page.clickNthFilterOperatorSelector(2);
+        browser.sleep(timegap);
+        page.selectNthMaterialOption(2);
+        browser.sleep(timegap);
+        page.clickNthFilterRHSSelector(2);
+        browser.sleep(timegap);
+        page.addSuggestionAndSelect(2, 10233);
+        browser.sleep(timegap);
+        page.selectNthRHS(2, 1);
+        browser.sleep(timegap);
+        page.clickApply(2);
+
+        browser.sleep(timegap);
+        page.clickAddButton();
+
+        filter = 3;
+        page.clickNthFilterFieldSelector(filter);
+        browser.sleep(timegap);
+        page.selectNthMaterialOption(1);
+        browser.sleep(timegap);
+        page.clickNthFilterOperatorSelector(filter);
+        browser.sleep(timegap);
+        page.selectNthMaterialOption(1);
+        browser.sleep(timegap);
+        page.clickNthFilterRHSSelector(filter);
+        browser.sleep(timegap);
+        page.selectNthRHS(filter, 1);
+        browser.sleep(timegap);
+        page.clickApply(filter);
+
+        browser.sleep(timegap);
+        page.clickAddButton();
+
+        filter = 4;
+        page.clickNthFilterFieldSelector(filter);
+        browser.sleep(timegap);
+        page.selectNthMaterialOption(3);
+        browser.sleep(timegap);
+        page.clickNthFilterOperatorSelector(filter);
+        browser.sleep(timegap);
+        page.selectNthMaterialOption(1);
+        browser.sleep(timegap);
+        page.clickNthFilterRHSSelector(filter);
+        browser.sleep(timegap);
+        page.enterInput(filter, 'Hello');
+
+        browser.sleep(timegap);
+        page.clickAddButton();
+        filter = 5;
+        page.clickNthFilterFieldSelector(filter);
+        browser.sleep(timegap);
+        page.selectNthMaterialOption(4);
+        browser.sleep(timegap);
+        page.clickNthFilterOperatorSelector(filter);
+        browser.sleep(timegap);
+        page.selectNthMaterialOption(1);
+        browser.sleep(timegap);
+        page.clickNthFilterRHSSelector(filter);
+        browser.sleep(timegap);
+        page.enterInput(filter, 2);
+
+        expect(page.getNthFilter(2)).not.toBeNull();
+    });
+
+    afterEach(async () => {
+        // Assert that there are no errors emitted from the browser
+        const logs = await browser
+            .manage()
+            .logs()
+            .get(logging.Type.BROWSER);
+        expect(logs).not.toContain(
+            jasmine.objectContaining({
+                level: logging.Level.SEVERE,
+            } as logging.Entry),
+        );
+    });
 });
